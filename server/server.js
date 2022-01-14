358f151089b27e9e017727c6f89b267ee1b24d4b
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-14 11:29:28 -0800

    install apollo server and auth

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const {ApolloServer} = require("apollo-server-express");
 const path = require('path');
 const db = require('./config/connection');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const routes = require('./routes');
 
 const app = express();
