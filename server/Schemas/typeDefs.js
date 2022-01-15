7205f52049c57e5dde1a238d8a3752305d6d967c
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-14 18:31:13 -0800

    update typeDefs

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -4,11 +4,41 @@ const typeDefs = gql`
 
 type Query {
     me: User
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
+    # users: [User]
+    # user(username: String!): User
+    # thoughts(username: String): [Thought]
+    # thought(_id: ID!): Thought
   }
 
 
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookText: String!): User
+    removeBook(bookId: ID!): User
+    # addFriend(friendId: ID!): User
+  }
+
+  type User {
+      _id: []
+      username:
+      email:
+      bookCount:
+      savedBooks: [Book]
+  }
+
+  type Book {
+      bookId:
+      authors:
+      description:
+      title:
+      image:
+      link:
+  }
+
+  type Auth {
+      token:
+      user: User
+  }
 `
+module.exports = typeDefs;
