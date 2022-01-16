ed426ddd3e29b00df59a7fdbdc0111a911263c99
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-15 16:39:03 -0800

    saveBook and defs

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -44,6 +44,24 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
           },
+
+          saveBook: async (parent, {bookData}, context) => {
+
+            console.log(context);
+            if (context.user) {
+              //const book = await Book.create({ ...args, username: context.user.username });
+      
+              const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: bookData } },
+                { new: true }
+              );
+                    
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError('You need to be logged in!');
+          },
     }
 
 
