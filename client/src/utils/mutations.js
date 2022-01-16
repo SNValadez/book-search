1638ed2cb9282f2b6fbaae102dabf718c9b565c9
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-15 19:55:49 -0800

    const REMOVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,3 +42,24 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        image
+        authors
+       
+    
+        title
+        link
+        description
+      }
+    }
+  }
+`;
